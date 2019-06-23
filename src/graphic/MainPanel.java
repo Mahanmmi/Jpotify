ac24bd145c8e088478ed1f86a1e9b2f27904a35e
Mahan Zendedel <43912482+Mahanmmi@users.noreply.github.com>
2019-06-23 14:03:25 +0430

    GUI Reworked

--- src/graphic/MainPanel.java
+++ src/graphic/MainPanel.java
@@ -1,8 +1,6 @@
 package graphic;
 
-import ch.randelshofer.quaqua.QuaquaButtonUI;
 import ch.randelshofer.quaqua.QuaquaManager;
-import ch.randelshofer.quaqua.jaguar.Quaqua15JaguarLookAndFeel;
 import logic.media.Media;
 import logic.media.MediaData;
 import logic.playlist.Playlist;
@@ -20,30 +18,34 @@ import java.util.Set;
 
 public class MainPanel implements PlaylistLinkable {
     private JPanel mainPanel;
-    private JPanel musicPanel;
     private JPanel listsPanel;
     private JPanel middlePanel;
     private JPanel friendPanel;
+    private JPanel upperPannel;
+    private JPanel bottomPanel;
+    private JPanel detailsPanel;
+    private JPanel autoPlaylistsPanel;
+    private JPanel volumePanel;
+    private JPanel musicButtonsPanel;
     private JTextArea textArea1;
     private JTextArea textArea2;
     private JTextArea textArea3;
-    private JTextArea refreshHandler;
     private JSlider volumeSlider;
-    private JButton lastTrackIcon;
-    private JButton volumeIcon;
-    private JButton nextTrackIcon;
+    private JButton lastTrackButton;
+    private JButton volumeButtun;
+    private JButton nextTrackButton;
     private JButton play_pause;
-    private JSlider musicSlider;
     private JButton musicName;
     private JButton artist;
     private JButton favorite;
     private JButton shared;
+    private JSlider musicSlider;
     private JFrame frame;
 
 
     private void initDarkTheme() {
         System.setProperty(
-                "Quaqua.design","lion"
+                "Quaqua.design", "lion"
 
         );
         Set<String> excludes = new HashSet<>();
@@ -64,50 +66,32 @@ public class MainPanel implements PlaylistLinkable {
         textArea2.setBackground(Color.lightGray);
         textArea3.setBackground(Color.lightGray);
 
-
-        mainPanel.setPreferredSize(new Dimension(frame.getWidth() - frame.getWidth() / 8, frame.getHeight() - frame.getHeight() / 8));
-        mainPanel.setMaximumSize(new Dimension(frame.getWidth() - frame.getWidth() / 8, frame.getHeight() - frame.getHeight() / 8));
-        mainPanel.setMinimumSize(new Dimension(frame.getWidth() - frame.getWidth() / 8, frame.getHeight() - frame.getHeight() / 8));
+        upperPannel.setMinimumSize(new Dimension(mainPanel.getWidth(), mainPanel.getHeight() * 4 / 6));
+        upperPannel.setMaximumSize(new Dimension(mainPanel.getWidth(), mainPanel.getHeight() * 4 / 6));
+        upperPannel.setPreferredSize(new Dimension(mainPanel.getWidth(), mainPanel.getHeight() * 4 / 6));
+        bottomPanel.setMinimumSize(new Dimension(mainPanel.getWidth() - 5, mainPanel.getHeight() / 6));
+        bottomPanel.setMaximumSize(new Dimension(mainPanel.getWidth() - 5, mainPanel.getHeight() / 6));
+        bottomPanel.setPreferredSize(new Dimension(mainPanel.getWidth() - 5, mainPanel.getHeight() / 6));
 
 
         mainPanel.setBackground(Color.BLACK);
-        musicPanel.setBackground(Color.BLACK);
         listsPanel.setBackground(Color.BLACK);
         middlePanel.setBackground(Color.darkGray);
         friendPanel.setBackground(Color.BLACK);
-
-
-        int width = mainPanel.getWidth() - mainPanel.getWidth() / 10;
-        int height = mainPanel.getHeight() - mainPanel.getHeight() / 10;
-
-        musicPanel.setPreferredSize(new Dimension(0, height / 5));
-        musicPanel.setMaximumSize(new Dimension(0, height / 5));
-        musicPanel.setMinimumSize(new Dimension(0, height / 5));
-
-        listsPanel.setPreferredSize(new Dimension(width / 5, (height * 5) / 7));
-        listsPanel.setMaximumSize(new Dimension(width / 5, (height * 5) / 7));
-        listsPanel.setMinimumSize(new Dimension(width / 5, (height * 5) / 7));
-
-        friendPanel.setPreferredSize(new Dimension(width / 5, (height * 5) / 7));
-        friendPanel.setMaximumSize(new Dimension(width / 5, (height * 5) / 7));
-        friendPanel.setMinimumSize(new Dimension(width / 5, (height * 5) / 7));
-
-        middlePanel.setPreferredSize(new Dimension((width * 3) / 5, (height * 5) / 7));
-        middlePanel.setMaximumSize(new Dimension((width * 3) / 5, (height * 5) / 7));
-        middlePanel.setMinimumSize(new Dimension((width * 3) / 5, (height * 5) / 7));
     }
 
     private void setMusicPanelIconsAndColors() {
-        nextTrackIcon.setIcon(new ImageIcon(new ImageIcon("./resources/nt.png").getImage().getScaledInstance(nextTrackIcon.getWidth() - 5, nextTrackIcon.getHeight() + 7, Image.SCALE_SMOOTH)));
-        lastTrackIcon.setIcon(new ImageIcon(new ImageIcon("./resources/lt.png").getImage().getScaledInstance(lastTrackIcon.getWidth() - 5, lastTrackIcon.getHeight() + 7, Image.SCALE_SMOOTH)));
-        volumeIcon.setIcon(new ImageIcon(new ImageIcon("./resources/v.png").getImage().getScaledInstance(volumeIcon.getWidth() - 5, volumeIcon.getHeight() + 7, Image.SCALE_SMOOTH)));
-        play_pause.setIcon(new ImageIcon(new ImageIcon("./resources/play.png").getImage().getScaledInstance(play_pause.getWidth() - 5, play_pause.getHeight() + 7, Image.SCALE_SMOOTH)));
-        nextTrackIcon.setBackground(Color.black);
-        lastTrackIcon.setBackground(Color.black);
-        play_pause.setBackground(Color.black);
-        volumeIcon.setBackground(Color.black);
+        nextTrackButton.setSize(nextTrackButton.getWidth(), nextTrackButton.getHeight() + 10);
+        nextTrackButton.setIcon(new ImageIcon(new ImageIcon("./resources/New Icons/Actions-media-seek-forward-icon.png").getImage().getScaledInstance(nextTrackButton.getHeight() + 4, nextTrackButton.getHeight() + 4, Image.SCALE_DEFAULT)));
 
+        lastTrackButton.setSize(lastTrackButton.getWidth(), lastTrackButton.getHeight() + 10);
+        lastTrackButton.setIcon(new ImageIcon(new ImageIcon("./resources/New Icons/Actions-media-seek-backward-icon.png").getImage().getScaledInstance(lastTrackButton.getHeight() + 4, lastTrackButton.getHeight() + 4, Image.SCALE_DEFAULT)));
 
+        volumeButtun.setSize(volumeButtun.getWidth(), volumeButtun.getHeight() + 10);
+        System.out.println(volumeButtun.getSize());
+        volumeButtun.setIcon(new ImageIcon(new ImageIcon("./resources/New Icons/speaker-icon.png").getImage().getScaledInstance(volumeButtun.getHeight() + 4, volumeButtun.getHeight() + 4, Image.SCALE_DEFAULT)));
+        play_pause.setSize(play_pause.getWidth(), play_pause.getHeight() + 10);
+        play_pause.setIcon(new ImageIcon(new ImageIcon("./resources/New Icons/Actions-media-playback-start-icon.png").getImage().getScaledInstance(play_pause.getHeight() + 4, play_pause.getHeight() + 4, Image.SCALE_DEFAULT)));
     }
 
     private JMenuBar initMenus() {
@@ -176,23 +160,31 @@ public class MainPanel implements PlaylistLinkable {
         newFrameInitialSettings(frame, mainPanel);
 
         frame.addWindowStateListener(event -> {
-            if (event.getNewState() == 6 || event.getOldState() == 6) {
-                updatePanelSizeAndColors();
-                try {
-                    Robot robot = new Robot();
-                    refreshHandler.grabFocus();
-                    robot.delay(50);
-                    robot.keyPress(KeyEvent.VK_0);
-                    robot.keyRelease(KeyEvent.VK_0);
-                    robot.keyPress(KeyEvent.VK_BACK_SPACE);
-                    robot.keyRelease(KeyEvent.VK_BACK_SPACE);
-                    frame.revalidate();
-                    mainPanel.revalidate();
-                    mainPanel.grabFocus();
-                } catch (AWTException e) {
-                    e.printStackTrace();
-                }
-            }
+            frame.setVisible(false);
+            updatePanelSizeAndColors();
+            mainPanel.repaint();
+            listsPanel.repaint();
+            middlePanel.repaint();
+            friendPanel.repaint();
+            upperPannel.repaint();
+            bottomPanel.repaint();
+            detailsPanel.repaint();
+            autoPlaylistsPanel.repaint();
+            volumePanel.repaint();
+            musicButtonsPanel.repaint();
+            frame.repaint();
+            mainPanel.validate();
+            listsPanel.validate();
+            middlePanel.validate();
+            friendPanel.validate();
+            upperPannel.validate();
+            bottomPanel.validate();
+            detailsPanel.validate();
+            autoPlaylistsPanel.validate();
+            volumePanel.validate();
+            musicButtonsPanel.validate();
+            frame.validate();
+            frame.setVisible(true);
         });
 
         frame.addComponentListener(new ComponentAdapter() {
