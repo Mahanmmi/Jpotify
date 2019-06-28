fd1fd18ac1a416fb76abecb96a18f315c112565e
mohammadjavadardestani <mjavad.ardestani00@gmail.com>
2019-06-29 02:16:32 +0430

    MediaData javaDoc added

--- src/logic/media/LyricsGatherer.java
+++ src/logic/media/LyricsGatherer.java
@@ -37,5 +37,4 @@ public class LyricsGatherer {
         }
         return lyrics;
     }
-
 }
--- src/logic/media/MediaData.java
+++ src/logic/media/MediaData.java
@@ -7,10 +7,14 @@ import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Date;
 
+/**
+ * this class use for saving data in files have a playList
+ * that contain playlistElement and lastPlayed
+ */
 public class MediaData implements Serializable {
     //its array list because a song might be in several play list
     private ArrayList<PlaylistElement> elements;
-
+   //its declare last time that song  played
     private Date lastPlayed;
 
     public Date getLastPlayed() {
@@ -21,6 +25,12 @@ public class MediaData implements Serializable {
         this.lastPlayed = lastPlayed;
     }
 
+    /**
+     * because oure sorting is comparative we declare a specified time for all song
+     * and compare them by distance of this date..
+     * @param address this is path of our song
+     * @param elements this declare that media is in which playList
+     */
     public MediaData(String address, ArrayList<PlaylistElement> elements) {
         this.elements = elements;
         lastPlayed = new Date(2000, Calendar.JANUARY,1);
